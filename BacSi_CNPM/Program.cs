783e4a34263773996b4eb4a0bbdbee63e0a6cf0a
21521832@gm.uit.edu.vn <21521832@gm.uit.edu.vn>
2023-11-27 16:49:38 +0700

    update DTO and DAT

--- BacSi_CNPM/Program.cs
+++ BacSi_CNPM/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Hospital.Views.Doctor;
+using Hospital.Views.Login;
 using Hospital.Views.Receptionist;
 
 namespace Hospital
@@ -18,7 +19,7 @@ namespace Hospital
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ReceptionForm());
+            Application.Run(new Login());
             //Application.Run(new Form2());
         }
     }
