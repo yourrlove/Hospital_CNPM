b390dc98b795ebf7a31f3d493230cbad4407613c
21521832@gm.uit.edu.vn <21521832@gm.uit.edu.vn>
2023-11-27 15:40:56 +0700

    Update DTO and DataAcessTier

---
+++ DataAccessTier/BenhNhanDBContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessTier
+{
+    public class BenhNhanDBContext : DBConnection
+    {
+        public DbSet<BenhNhan> BenhNhan { get; set; }
+        public BenhNhanDBContext() { }
+
+        public static BindingList<BenhNhan>? GetListPatient()
+        {
+            try
+            {
+                var dbContext = new BenhNhanDBContext();
+                dbContext.BenhNhan.Load();
+                return  dbContext.BenhNhan.Local.ToBindingList();
+
+            } catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+    }
+}
